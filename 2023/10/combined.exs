bd356872e01b0a97e30c143a5189e32815de6cb0
Adrian Irving-Beer <wisq@wisq.net>
2023-12-10 22:31:11 -0500

    Solve day 10 part 2.

--- 2023/10/combined.exs
+++ 2023/10/combined.exs
@@ -30,6 +30,15 @@ defmodule Pipes do
     "S" => %Segment{is_start: true}
   }
 
+  @inverse_symbols @symbols
+                   |> Enum.filter(fn
+                     {_, %Segment{connections: [_, _]}} -> true
+                     _ -> false
+                   end)
+                   |> Map.new(fn {symbol, %Segment{connections: conns}} ->
+                     {Enum.sort(conns), symbol}
+                   end)
+
   @neighbour_directions [
     north,
     south,
@@ -38,11 +47,32 @@ defmodule Pipes do
   ]
 
   def run do
-    load_grid()
-    |> connect_start()
-    |> IO.inspect(label: "Grid")
-    |> find_path_length()
-    |> find_inner_area()
+    grid =
+      load_grid()
+      |> connect_start()
+      |> inspect_grid()
+
+    path =
+      grid
+      |> path_stream()
+      |> Enum.to_list()
+      |> IO.inspect(label: "Path")
+
+    path
+    |> Enum.count()
+    |> div(2)
+    |> IO.inspect(label: "Half of path length")
+
+    grid
+    |> delete_non_path_segments(path)
+    |> inflate()
+    |> flood_fill_outer_cells()
+    |> inspect_grid()
+    |> Enum.count(fn
+      {_, :ground} -> true
+      {_, _} -> false
+    end)
+    |> IO.inspect(label: "Area of inner ground")
   end
 
   defp load_grid do
@@ -60,19 +90,6 @@ defmodule Pipes do
     |> Map.new()
   end
 
-  defp find_path_length(grid) do
-    grid
-    |> loop_stream()
-    |> Enum.to_list()
-    |> IO.inspect(label: "Loop")
-    |> Enum.count()
-    |> div(2)
-    |> IO.inspect(label: "Half of length")
-  end
-
-  defp find_inner_area(grid) do
-  end
-
   defp connect_start(grid) do
     coords = find_start_coords(grid)
 
@@ -82,7 +99,7 @@ defmodule Pipes do
     end)
   end
 
-  defp loop_stream(grid) do
+  defp path_stream(grid) do
     start_coords = find_start_coords(grid)
 
     Stream.resource(
@@ -126,6 +143,127 @@ defmodule Pipes do
       case Map.get(grid, neighbour_coords) do
         %Segment{connections: conns} -> inverse in conns
         :ground -> false
+        nil -> false
+      end
+    end)
+  end
+
+  defp delete_non_path_segments(grid, path) do
+    path = MapSet.new(path)
+
+    grid
+    |> Map.new(fn
+      {coord, :ground} ->
+        {coord, :ground}
+
+      {coord, %Segment{} = segment} ->
+        case coord in path do
+          true -> {coord, segment}
+          false -> {coord, :ground}
+        end
+    end)
+  end
+
+  defp inflate(grid) do
+    grid =
+      grid
+      |> Map.new(fn {{row, col}, cell} ->
+        {{row * 2, col * 2}, cell}
+      end)
+
+    {{max_row, max_col}, _} = Enum.max(grid)
+
+    existing_coords = Map.keys(grid) |> MapSet.new()
+
+    new_coords =
+      0..max_row
+      |> Enum.flat_map(fn row ->
+        0..max_col
+        |> Enum.map(fn col -> {row, col} end)
+      end)
+      |> MapSet.new()
+      |> MapSet.difference(existing_coords)
+
+    new_coords
+    |> Map.new(fn coord ->
+      case find_connections(grid, coord) do
+        [] -> {coord, :filler}
+        [_ | _] = conns -> {coord, %Segment{connections: conns}}
+      end
+    end)
+    |> Map.merge(grid)
+  end
+
+  defp inspect_grid(grid) do
+    grid
+    |> Enum.group_by(fn {{row, _}, _} -> row end)
+    |> Enum.sort()
+    |> Enum.map(fn {_, row} ->
+      row
+      |> Enum.sort()
+      |> Enum.map(fn
+        {_, %Segment{connections: conns}} -> Map.fetch!(@inverse_symbols, Enum.sort(conns))
+        {_, :ground} -> "."
+        {_, :filler} -> " "
+        {_, :outer} -> "O"
+      end)
+      |> then(fn row -> [row, "\n"] end)
+    end)
+    |> IO.puts()
+
+    grid
+  end
+
+  defp flood_fill_outer_cells(grid) do
+    {{max_row, max_col}, _} = Enum.max(grid)
+
+    outer_coords =
+      [
+        0..max_col |> Enum.map(fn col -> {0, col} end),
+        0..max_col |> Enum.map(fn col -> {max_row, col} end),
+        0..max_row |> Enum.map(fn row -> {row, 0} end),
+        0..max_row |> Enum.map(fn row -> {row, max_col} end)
+      ]
+      |> List.flatten()
+      |> Enum.uniq()
+      |> Enum.filter(fn coord ->
+        Map.get(grid, coord) == :ground
+      end)
+      |> MapSet.new()
+
+    grid
+    |> recursive_fill(outer_coords, :outer)
+  end
+
+  defp recursive_fill(grid, fill_coords, new_cell) do
+    fill_map =
+      fill_coords
+      |> Map.new(fn coords -> {coords, new_cell} end)
+
+    neighbour_coords =
+      fill_coords
+      |> Enum.flat_map(fn coords ->
+        @neighbour_directions
+        |> Enum.map(&Coords.add(coords, &1))
+        |> Enum.reject(&(&1 in fill_coords))
+        |> Enum.filter(fn coords ->
+          case Map.get(grid, coords) do
+            :filler -> true
+            :ground -> true
+            ^new_cell -> false
+            nil -> false
+            %Segment{} -> false
+          end
+        end)
+      end)
+      |> MapSet.new()
+
+    grid
+    |> Map.merge(fill_map)
+    |> then(fn grid ->
+      case Enum.empty?(neighbour_coords) do
+        true -> grid
+        false -> recursive_fill(grid, neighbour_coords, new_cell)
       end
     end)
   end

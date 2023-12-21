455a31985fcc78755d8d9ce357ffd6aa675633ce
Adrian Irving-Beer <wisq@wisq.net>
2023-12-20 22:58:19 -0500

    Add part 2.

---
+++ 2023/20/b.exs
@@ -0,0 +1,269 @@
+defmodule Pulses do
+  defmodule Broadcaster do
+    @enforce_keys [:targets]
+    defstruct(@enforce_keys)
+  end
+
+  defmodule FlipFlop do
+    @enforce_keys [:targets]
+    defstruct(
+      targets: nil,
+      on: false
+    )
+  end
+
+  defmodule Conjunction do
+    @enforce_keys [:targets]
+    defstruct(
+      targets: nil,
+      memory: %{}
+    )
+  end
+
+  defmodule LCMConjunction do
+    @enforce_keys [:pulses]
+    defstruct(pulses: %{})
+  end
+
+  defmodule State do
+    @enforce_keys [:layout, :pushes]
+    defstruct(
+      pushes: 0,
+      layout: nil,
+      pending: :queue.new(),
+      lows: 0,
+      highs: 0,
+      solution: nil
+    )
+  end
+
+  def run do
+    parse_layout()
+    |> populate_memory()
+    |> update_critical_node()
+    |> IO.inspect()
+    |> push_button(1)
+  end
+
+  defp parse_layout do
+    IO.stream(:stdio, :line)
+    |> Map.new(fn line ->
+      line
+      |> String.trim()
+      |> parse_module()
+    end)
+  end
+
+  defp parse_module("broadcaster -> " <> targets) do
+    {:broadcaster,
+     %Broadcaster{
+       targets: targets |> parse_targets()
+     }}
+  end
+
+  defp parse_module("%" <> flipflop) do
+    [name, targets] = String.split(flipflop, " -> ")
+
+    {name |> String.to_atom(),
+     %FlipFlop{
+       targets: targets |> parse_targets()
+     }}
+  end
+
+  defp parse_module("&" <> conjunction) do
+    [name, targets] = String.split(conjunction, " -> ")
+
+    {name |> String.to_atom(),
+     %Conjunction{
+       targets: targets |> parse_targets()
+     }}
+  end
+
+  defp parse_targets(targets) do
+    targets
+    |> String.split(",")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.to_atom/1)
+  end
+
+  defp populate_memory(layout) do
+    layout
+    |> Map.new(fn {name, module} ->
+      {name, populate(name, module, layout)}
+    end)
+  end
+
+  defp populate(name, %Conjunction{} = module, layout) do
+    memory =
+      reverse_lookup(layout, name)
+      |> Map.new(fn {n, _} -> {n, :low} end)
+
+    %Conjunction{module | memory: memory}
+  end
+
+  defp populate(name, module, _layout), do: module
+
+  def push_button(layout, pushes) do
+    %State{layout: layout, pushes: pushes}
+    |> send_pulse(:button, :low, [:broadcaster])
+    |> execute_pending()
+    |> then(fn
+      %State{solution: nil} = state ->
+        state.layout
+        |> push_button(pushes + 1)
+
+      %State{solution: n} = state when is_integer(n) ->
+        state
+        |> IO.inspect(label: "Final state")
+    end)
+  end
+
+  defp send_pulse(state, source, type, targets) do
+    count = Enum.count(targets)
+
+    state =
+      case type do
+        :low -> %State{state | lows: state.lows + count}
+        :high -> %State{state | highs: state.highs + count}
+      end
+
+    event = {source, type, targets}
+    pending = :queue.in(event, state.pending)
+    %State{state | pending: pending}
+  end
+
+  defp execute_pending(state) do
+    pending = :queue.to_list(state.pending)
+    state = %State{state | pending: :queue.new()}
+
+    pending
+    |> Enum.reduce(state, fn {source, type, targets}, state ->
+      targets
+      |> Enum.reduce(state, fn target, state ->
+        case Map.fetch(state.layout, target) do
+          {:ok, module} -> %State{} = handle_pulse(state, module, target, source, type)
+          :error -> state
+        end
+      end)
+    end)
+    |> then(fn state ->
+      case :queue.is_empty(state.pending) do
+        true -> state
+        false -> execute_pending(state)
+      end
+    end)
+  end
+
+  defp handle_pulse(
+         state,
+         %Broadcaster{} = module,
+         :broadcaster = name,
+         :button = source,
+         :low = type
+       ) do
+    state
+    |> send_pulse(name, type, module.targets)
+  end
+
+  defp handle_pulse(state, %FlipFlop{} = module, _name, _source, :high) do
+    state
+  end
+
+  defp handle_pulse(state, %FlipFlop{on: on} = module, name, _source, :low) do
+    state
+    |> update_module(name, %FlipFlop{module | on: !on})
+    |> send_pulse(
+      name,
+      case on do
+        false -> :high
+        true -> :low
+      end,
+      module.targets
+    )
+  end
+
+  defp handle_pulse(state, %Conjunction{} = module, name, source, type) do
+    module = %Conjunction{module | memory: Map.put(module.memory, source, type)}
+
+    state
+    |> update_module(name, module)
+    |> send_pulse(
+      name,
+      case module.memory |> Enum.all?(fn {_src, t} -> t == :high end) do
+        true -> :low
+        false -> :high
+      end,
+      module.targets
+    )
+  end
+
+  defp handle_pulse(state, %LCMConjunction{} = module, name, source, :high) do
+    pulses =
+      module.pulses
+      |> Map.update!(
+        source,
+        fn
+          nil ->
+            {:measuring, state.pushes}
+
+          {:measuring, old} ->
+            case old * 2 == state.pushes do
+              true -> {:locked, old}
+            end
+
+          {:locked, old} ->
+            case rem(state.pushes, old) == 0 do
+              true -> {:locked, old}
+            end
+        end
+      )
+      |> IO.inspect(label: inspect(name))
+
+    state = state |> update_module(name, %LCMConjunction{module | pulses: pulses})
+
+    case Enum.all?(pulses, fn
+           {_, {:locked, _}} -> true
+           {_, _} -> false
+         end) do
+      true -> %State{state | solution: solve_lcm_conjunction(pulses)}
+      false -> state
+    end
+  end
+
+  defp handle_pulse(state, %LCMConjunction{} = module, name, source, :low) do
+    state
+  end
+
+  defp solve_lcm_conjunction(pulses) do
+    pulses
+    |> Enum.map(fn {_, {:locked, n}} -> n end)
+    |> Enum.reduce(&lcm/2)
+  end
+
+  defp lcm(a, b) do
+    div(a * b, Integer.gcd(a, b))
+  end
+
+  defp update_module(state, name, module) do
+    layout = state.layout |> Map.put(name, module)
+    %State{state | layout: layout}
+  end
+
+  defp reverse_lookup(layout, name) do
+    layout
+    |> Enum.filter(fn {_, %{targets: targets}} -> name in targets end)
+  end
+
+  defp update_critical_node(layout) do
+    [{name, %Conjunction{memory: memory}}] = reverse_lookup(layout, :rx)
+
+    module = %LCMConjunction{
+      pulses: memory |> Map.new(fn {key, _} -> {key, nil} end)
+    }
+
+    layout
+    |> Map.put(name, module)
+  end
+end
+
+Pulses.run()

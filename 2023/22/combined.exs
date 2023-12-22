060c0a14ca61730ab24886f33e63555aa9661e12
Adrian Irving-Beer <wisq@wisq.net>
2023-12-22 12:19:05 -0500

    Add part 2 solution to combined day 22 solver.

--- 2023/22/combined.exs
+++ 2023/22/combined.exs
@@ -1,17 +1,30 @@
 defmodule Stacker do
+  defmodule Brick do
+    @enforce_keys [:id, :coords, :footprint, :lowest_z]
+    defstruct(@enforce_keys)
+  end
+
   def run do
     bricks = parse_bricks()
-    world = bricks |> Enum.reduce(&MapSet.union/2)
+    world = bricks |> Enum.map(& &1.coords) |> Enum.reduce(&MapSet.union/2)
 
     {bricks, world} = fall_until_stable(bricks, world)
+    zap_drops = count_zap_drops(bricks, world)
 
-    count_zappable(bricks, world)
-    |> IO.inspect(label: "Zappable bricks")
+    zap_drops
+    |> Enum.filter(&(&1 == 0))
+    |> Enum.count()
+    |> IO.inspect(label: "Safely zappable bricks")
+
+    zap_drops
+    |> Enum.sum()
+    |> IO.inspect(label: "Zap chain reactions")
   end
 
   def parse_bricks do
     IO.stream(:stdio, :line)
-    |> Enum.map(fn line ->
+    |> Enum.with_index(1)
+    |> Enum.map(fn {line, index} ->
       line
       |> String.trim()
       |> String.split("~")
@@ -22,8 +35,15 @@ defmodule Stacker do
         |> then(fn [x, y, z] -> {x, y, z} end)
       end)
       |> then(fn [head, tail] ->
-        brick_coords(head, tail)
-        |> MapSet.new()
+        coords = brick_coords(head, tail)
+        {footprint, lowest_z} = get_footprint(coords)
+
+        %Brick{
+          id: index |> Integer.to_string() |> String.pad_leading(4, "0"),
+          coords: coords |> MapSet.new(),
+          footprint: footprint,
+          lowest_z: lowest_z
+        }
       end)
     end)
     |> MapSet.new()
@@ -35,8 +55,6 @@ defmodule Stacker do
   defp brick_coords({x, y, z1}, {x, y, z2}), do: z1..z2 |> Enum.map(fn z -> {x, y, z} end)
 
   defp fall_until_stable(bricks, world) do
-    IO.puts("Falling ...")
-
     case fall_once(bricks, world) do
       {^bricks, ^world} -> {bricks, world}
       {bricks, world} -> fall_until_stable(bricks, world)
@@ -51,14 +69,14 @@ defmodule Stacker do
     end)
   end
 
-  defp attempt_fall(brick, world) do
-    {footprint, z} = get_footprint(brick)
+  defp attempt_fall(%Brick{} = brick, world) do
+    z = brick.lowest_z
 
     stop_at =
       z..1//-1
       |> Enum.find(fn
         1 -> true
-        z -> footprint |> Enum.any?(fn {x, y} -> {x, y, z - 1} in world end)
+        z -> brick.footprint |> Enum.any?(fn {x, y} -> {x, y, z - 1} in world end)
       end)
 
     case stop_at do
@@ -67,51 +85,61 @@ defmodule Stacker do
     end
   end
 
-  defp get_footprint(brick) do
+  defp get_footprint(coords) do
     lowest_z =
-      brick
+      coords
       |> Enum.map(fn {_, _, z} -> z end)
       |> Enum.min()
 
     footprint =
-      brick
+      coords
       |> Enum.filter(fn {_, _, z} -> z == lowest_z end)
       |> Enum.map(fn {x, y, ^lowest_z} -> {x, y} end)
 
     {footprint, lowest_z}
   end
 
-  defp drop_brick(old_brick, by_z, old_world) do
-    IO.puts("Dropping #{inspect(old_brick)} by #{by_z} blocks")
+  defp drop_brick(%Brick{} = old_brick, by_z, old_world) do
+    # IO.puts("Dropping #{old_brick.id} by #{by_z} blocks")
 
     new_brick =
-      old_brick
-      |> Enum.map(fn {x, y, z} -> {x, y, z - by_z} end)
-      |> MapSet.new()
+      %Brick{
+        old_brick
+        | coords:
+            old_brick.coords
+            |> Enum.map(fn {x, y, z} -> {x, y, z - by_z} end)
+            |> MapSet.new(),
+          lowest_z: old_brick.lowest_z - by_z
+      }
 
     new_world =
       old_world
-      |> MapSet.difference(old_brick)
-      |> MapSet.union(new_brick)
+      |> MapSet.difference(old_brick.coords)
+      |> MapSet.union(new_brick.coords)
 
     {new_brick, new_world}
   end
 
-  defp count_zappable(bricks, world) do
-    IO.puts("Counting zappable bricks ...")
+  defp count_zap_drops(bricks, world) do
+    IO.puts("Counting zappable bricks & their respective drops ...")
 
     bricks
-    |> Enum.count(&can_zap?(&1, bricks, world))
+    |> Enum.sort_by(& &1.id)
+    |> Enum.map(&count_brick_zap_drops(&1, bricks, world))
   end
 
-  defp can_zap?(brick, bricks, world) do
-    IO.puts("Zapping brick #{inspect(brick)} ...")
+  defp count_brick_zap_drops(brick, bricks, world) do
+    IO.puts("Zapping brick #{brick.id} ...")
     bricks = MapSet.delete(bricks, brick)
-    world = MapSet.difference(world, brick)
+    world = MapSet.difference(world, brick.coords)
 
-    case fall_once(bricks, world) do
-      {^bricks, ^world} -> true
-      {_, _} -> false
+    case fall_until_stable(bricks, world) do
+      {^bricks, ^world} ->
+        0
+
+      {new_bricks, _} ->
+        MapSet.difference(bricks, new_bricks)
+        |> Enum.count()
     end
   end
 end
